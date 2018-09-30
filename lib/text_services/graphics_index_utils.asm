b57f4aed96b2e19fbdc160de2852a6f8a8064ab9
David Wendt <dcrkid@yahoo.com>
2018-09-30 14:12:05 -0600

    Here's some utilities for cursoring around graphics tiles.

---
+++ lib/text_services/graphics_index_utils.asm
@@ -0,0 +1,114 @@
+INCLUDE "lib/brewbase.inc"
+
+SECTION "Text Services - Graphics Index Utils", ROM0
+;Get the location of the window's current tile
+;HL = Window structure
+;BC = X and Y tile offsets
+;Returns HL = Tile pointer
+TextServices_GetWindowCursorTile:
+    push de
+    
+    ;M_TextServices_WindowWidth is $0
+    ld a, [hl]
+    push af
+    
+    ld de, M_TextServices_WindowBacking
+    add hl, de
+    ld a, [hli]
+    ld h, [hl]
+    ld l, a
+    
+    pop af
+    call TextServices_IncrementLines
+    ld a, b
+    call TextServices_IncrementTiles
+    
+    pop de
+    ret
+
+;Increment by some number of tiles
+;A = Number of tiles
+;HL = Current tile pointer
+;Returns HL = New tile pointer
+TextServices_IncrementByTiles:
+    push de
+    
+    ld e, 0
+    sla a
+    rl e
+    sla a
+    rl e
+    sla a
+    rl e
+    sla a
+    rl e
+    ld d, a
+    add hl, de
+    
+    pop de
+    ret
+
+;Increment by some number of tile rows
+;A = Number of tiles in a row
+;C = Number of rows to increment
+;HL = Current tile pointer
+;Returns HL = New tile pointer
+TextServices_IncrementByTileRows:
+    push de
+    
+    ld e, 0
+    sla a
+    rl e
+    sla a
+    rl e
+    sla a
+    rl e
+    sla a
+    rl e
+    ld d, a
+    
+.y_mul_loop
+    dec c
+    jr c, .done_y_mul
+    add hl, de
+    jr .y_mul_loop
+    
+.done_y_mul
+    pop de
+    ret
+
+;Index by a number of glyphs
+;A = Number of glyphs
+;B = Glyph width / 8
+;C = Glyph height
+;HL = Glyph base pointer
+;Returns HL = Individual glyph pointer
+TextServices_IndexGlyphs:
+    push de
+    push hl
+    
+    ld h, 0
+    ld l, 0
+    ld d, 0
+    ld e, c
+    
+.glyph_mul_loop
+    dec b
+    jr c, .done_glyph_mul
+    add hl, de
+    jr .glyph_mul_loop
+
+.done_glyph_mul
+    ld d, h
+    ld e, l
+    pop hl
+    
+.index_mul_loop
+    dec a
+    jr c, .done_index_mul
+    add hl, de
+    jr .index_mul_loop
+    
+.done_index_mul
+    pop de
+    ret
