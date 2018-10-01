6b1aee0437e7e65f43ee66494d690228aba8e9f9
David Wendt <dcrkid@yahoo.com>
2018-09-30 22:21:43 -0600

    Since we have an actual struct member called size, we can't call the struct size that

--- lib/lcd_control/valloc.asm
+++ lib/lcd_control/valloc.asm
@@ -1,4 +1,4 @@
 INCLUDE "lib/brewbase.inc"
 
 SECTION "Valloc Metadata Memory", WRAM0
-W_LCDC_VallocArena:: ds M_LCDC_VallocSize * M_LCDC_VallocCount
+W_LCDC_VallocArena:: ds M_LCDC_VallocStructSize * M_LCDC_VallocCount
