4fa0e9c0e7421d70ec99d77e3bfc3cc0ed48c702
David Wendt <dcrkid@yahoo.com>
2018-11-18 15:28:57 -0700

    Merge NDMA emulation into the main "process everything in chunks" routine.

--- lib/lcd_control/ndma.asm
+++ lib/lcd_control/ndma.asm
@@ -49,6 +49,11 @@ LCDC_ExecuteCurrentNDMAEntry::
 .no_cap_transfer_rate
     ld c, a
     
+    ld a, [W_System_ARegStartup]
+    cp M_BIOS_CPU_CGB
+    jr nz, .use_emulated_ndma
+    
+.use_real_ndma
     ld a, [hli]
     ld [REG_HDMA2], a   ;For some reason the NDMA hardware is big endian!?
     
@@ -75,7 +80,52 @@ LCDC_ExecuteCurrentNDMAEntry::
     dec a
     and $7F
     ld [REG_HDMA5], a   ;Trigger the actual NDMA transfer
+    jr .update_entry_status
     
+.use_emulated_ndma
+    push bc
+    push de
+    
+    push hl
+    inc hl
+    inc hl
+    
+    ;Lock the ROM/WRAM banks to the specified source bank for the duration of
+    ;NDMA transfer.
+    ld a, [hli]
+    ld [$2000], a
+    ld [REG_SVBK], a
+    
+    ld a, [hli]
+    ld e, a
+    ld a, [hli]
+    ld d, a
+    
+    ;Also lock the VRAM bank to the destination bank.
+    ld a, [hli]
+    ld [REG_VBK], a
+    
+    pop hl
+    ld a, [hli]
+    ld h, [hl]
+    ld l, a
+    
+.copy_loop ;TODO: Can this be faster?
+    REPT 16
+    ld a, [hli]
+    ld [de], a
+    inc de
+    ENDR
+    
+    dec c
+    jr nz, .copy_loop
+    
+    pop de
+    pop bc
+    
+    ld a, c
+    
+.update_entry_status
     ;At this point, we need to update the entry
     ld hl, W_LCDC_CurrentVallocEntry + M_LCDC_VallocSize
     sub [hl]
@@ -136,10 +186,6 @@ LCDC_ExecuteCurrentNDMAEntry::
 ;This routine clobbers REG_VBK, we assume all VRAM access will occur using this
 ;routine. If not the case, you will need to use another routine.
 LCDC_ResolvePendingNDMA::
-    ld a, [W_System_ARegStartup]
-    cp M_BIOS_CPU_CGB
-    jr nz, LCDC_EmulatePendingNDMA
-    
     ld c, 0     ;current arena entry
     ld b, M_LCDC_NDMARequestProcessingCap ;total processing time remaining
     ld hl, W_LCDC_VallocArena
@@ -198,112 +244,6 @@ LCDC_ResolvePendingNDMA::
     jr nc, .processEntry
     inc h
     jr .processEntry
-    
-.exitDMAProcessing
-    ;Unclobber banks. TODO: What of REG_VBK?
-    ld a, [H_System_CurBank]
-    ld [$2000], a
-    ld a, [H_System_CurRamBank]
-    ld [REG_SVBK], a
-    
-    ret
-    
-;Okay. Someone just plopped your fancy new homebrew cart into a disgustingly
-;ancient brick DMG. How are you gonna get your graphics onto it's creamed
-;spinach display? With this bad boy.
-;
-;TODO: Add a facility to split emulated NDMA workloads across multiple frames.
-;Currently, if you have a Valloc longer than the processing cap it won't get
-;processed at all.
-LCDC_EmulatePendingNDMA::
-    ld d, 0     ;current arena entry
-    ld c, M_LCDC_NDMARequestEmulatedProcessingCap    ;total processing time remaining
-    ld hl, W_LCDC_VallocArena
-    
-.processEntry
-    ld a, [hl]
-    cp M_LCDC_VallocStatusDirty
-    jr nz, .skipDMAEntry
-    
-    push hl
-    inc hl
-    push hl
-    
-    ;Check if servicing this request would exceed the NDMA cap
-    ld a, [hli]
-    cp c
-    jr nc, .exitDMAProcessingFromStack
-    
-    ;At this point we're ready to start talking to the NDMA hardware...
-    ;oh wait no lets emulate it instead
-.timeRemains
-    inc hl
-    inc hl
-    
-    ;Lock the ROM/WRAM banks to the specified source bank for the duration of
-    ;NDMA transfer.
-    ld a, [hli]
-    ld [$2000], a
-    ld [REG_SVBK], a
-    
-    ld a, [hli]
-    ld e, a
-    ld a, [hli]
-    ld d, a
-    
-    ;Also lock the VRAM bank to the destination bank.
-    ld a, [hli]
-    ld [REG_VBK], a
-    
-    pop hl
-    ld a, [hli]
-    ld b, a
-    inc b
-    push bc
-    
-    ld a, [hli]
-    ld h, [hl]
-    ld l, a
-    
-.copy_loop ;TODO: Can this be faster?
-    REPT 16
-    ld a, [hli]
-    ld [de], a
-    inc de
-    ENDR
-    
-    dec b
-    jr nz, .copy_loop
-    
-.done_copying
-    ;Subtract from our processing cap.
-    pop af
-    cpl
-    inc a
-    add c
-    ld c, a
-    
-.transfer_complete
-    ;Mark the transfer as completed.
-    pop hl
-    ld a, M_LCDC_VallocStatusClean
-    ld [hl], a
-    
-.skipDMAEntry
-    inc d
-    ld a, d
-    cp M_LCDC_VallocCount
-    jr z, .exitDMAProcessing
-    
-    ld a, l
-    add M_LCDC_VallocStructSize
-    ld l, a
-    jr nc, .processEntry
-    inc h
-    jr .processEntry
-    
-.exitDMAProcessingFromStack
-    add sp, 4
     
 .exitDMAProcessing
     ;Unclobber banks. TODO: What of REG_VBK?
