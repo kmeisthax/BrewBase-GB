d2c2636fe02bd25bc8f3ba9e7d1f4de064702199
David Wendt <dcrkid@yahoo.com>
2018-11-24 23:06:56 -0700

    Optimize the farcall code. Banking now constructs a small routine in HRAM and jumps to it to do the actual call. Restricted banking still exists, and is slightly faster, but I'm not sure how long it'll last!

--- lib/system/bank.asm
+++ lib/system/bank.asm
@@ -1,12 +1,10 @@
 INCLUDE "lib/brewbase.inc"
 
-SECTION "PBase Bank Switch Services Memory", HRAM
+SECTION "PBase Bank Switch Services High Memory", HRAM
 H_System_CurBank:: ds 1
 H_System_CurRamBank:: ds 1
 
-H_System_RegReserveA:: ds 1
-H_System_RegReserveH:: ds 1
-H_System_RegReserveL:: ds 1
+H_System_FarcallTrampoline:: ds 8 ;call $1234
 H_System_LastBank:: ds 1
 
 SECTION "PBase RST Bank Services", ROM0[$0000]
@@ -45,19 +43,37 @@ SECTION "PBase Bank Switch Services Initialize", ROM0
 ;Initialize the bank system, if present, to bank 1.
 ;We avoid the use of the potentially invalid bank 0.
 System_BankInit::
+    ;These are z80 opcodes necessary for a ld a/ld hl/jp trampoline to work.
+    ;Writing these at the start of the program saves 48 cycles per farcall.
+    ld a, $3E
+    ld [H_System_FarcallTrampoline + 0], a
+    ld a, $21
+    ld [H_System_FarcallTrampoline + 2], a
+    ld a, $C3
+    ld [H_System_FarcallTrampoline + 5], a
+    
     ld a, 1
     ld [H_System_CurBank], a
     ld [$2000], a
     ret
 
 System_FarCall_int::
-    ld [H_System_RegReserveA], a
-    ld a, h
-    ld [H_System_RegReserveH], a
+    ;Store the A and HL parameters in an HRAM instruction stream
+    ;Executing the SMC stream will restore A and HL
+    ld [H_System_FarcallTrampoline + 1], a
     ld a, l
-    ld [H_System_RegReserveL], a
+    ld [H_System_FarcallTrampoline + 3], a
+    ld a, h
+    ld [H_System_FarcallTrampoline + 4], a
     pop hl
     
+    ;SMC: jp $nnnn, where nnnn is copied from the instruction stream
+    ld a, [hli]
+    ld [H_System_FarcallTrampoline + 6], a
+    ld a, [hli]
+    ld [H_System_FarcallTrampoline + 7], a
+    
+    di
     ;Switch banks
     ld a, [H_System_CurBank]
     ld [H_System_LastBank], a
@@ -65,32 +81,22 @@ System_FarCall_int::
     ld a, [hli] ;Farcall bank ptr
     ld [H_System_CurBank], a
     ld [$2000], a
+    ei
     push hl
     
     ld a, [H_System_LastBank]
     push af
-    
-    ld a, [hli] ;Farcall function ptr
-    ld l, [hl]
-    ld h, a
-    call .farjmp
+    call H_System_FarcallTrampoline
     
     ;Preserve A and HL for return.
-    ld [H_System_RegReserveA], a
-    ld a, h
-    ld [H_System_RegReserveH], a
-    ld a, l
-    ld [H_System_RegReserveL], a
+    ld [H_System_FarcallTrampoline + 1], a
     
     pop af
     ld [H_System_CurBank], a
     ld [$2000], a
     
-    pop hl
-    inc hl
-    inc hl
-.farjmp
-    jp hl
+    ld a, [H_System_FarcallTrampoline + 1]
+    ret
 
 System_FarCallRestricted_int::
     pop hl
@@ -102,7 +108,6 @@ System_FarCallRestricted_int::
     ld a, [hli] ;Farcall bank ptr
     ld [H_System_CurBank], a
     ld [$2000], a
-    push hl
     
     ld a, [H_System_LastBank]
     push af
@@ -110,15 +115,13 @@ System_FarCallRestricted_int::
     ld a, [hli] ;Farcall function ptr
     ld l, [hl]
     ld h, a
+    push hl
     call .farjmp
     
+    pop hl
     pop af
     ld [H_System_CurBank], a
     ld [$2000], a
-    
-    pop hl
-    inc hl
-    inc hl
 .farjmp
     jp hl
 
@@ -137,7 +140,7 @@ System_FarSnap_int::
     ld [$2000], a
     
     ld a, [hli]
-    ld [H_System_RegReserveA], a
+    ld [H_System_FarcallTrampoline + 1], a
     ld a, [hli]
     ld h, [hl]
     ld l, a
@@ -145,7 +148,7 @@ System_FarSnap_int::
     ld a, [H_System_CurBank]
     ld [$2000], a
     
-    ld a, [H_System_RegReserveA]
+    ld a, [H_System_FarcallTrampoline + 1]
     ret
 
 System_FarCopy_int::
