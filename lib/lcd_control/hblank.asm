63206f861ddf5ba0f1a691b11b6c2aed3beefad5
David Wendt <dcrkid@yahoo.com>
2018-12-21 01:23:01 -0500

    Switch over to a yield macro rather than a callable.

--- lib/lcd_control/hblank.asm
+++ lib/lcd_control/hblank.asm
@@ -1,10 +1,9 @@
 INCLUDE "lib/brewbase.inc"
 
-SECTION "BBase HBlank Services Memory", WRAM0
-W_LCDC_HBlankInstr:: ds 3 ;Hblank handler.
-                          ;Always holds a jp to the current code.
 SECTION "BBase HBlank Services High Memory", HRAM
 H_LCDC_HBlankARegPreserve:: ds 1
+H_LCDC_HBlankInstr:: ds 3 ;Hblank handler.
+                          ;Always holds a jp to the current code.
 
 SECTION "BBase HBlank Services IRQ Handler", ROM0[$0048]
 ;The actual STAT interrupt handler.
@@ -33,7 +32,7 @@ SECTION "BBase HBlank Services IRQ Handler", ROM0[$0048]
 ;HBlank handler may have less time in order to accomplish the same task.
 LCDC_StatIRQ::
     push af
-    jp W_LCDC_HBlankInstr
+    jp H_LCDC_HBlankInstr
 
 SECTION "LCDC H-Blank Utilities", ROM0
 ;Install a given StatIRQ handler into HBlankInstr.
@@ -50,31 +49,14 @@ LCDC_HBlankInit::
     push af
     
     ld a, $C3
-    ld [W_LCDC_HBlankInstr], a
+    ld [H_LCDC_HBlankInstr], a
     ld a, l
-    ld [W_LCDC_HBlankInstr + 1], a
+    ld [H_LCDC_HBlankInstr + 1], a
     ld a, h
-    ld [W_LCDC_HBlankInstr + 2], a
+    ld [H_LCDC_HBlankInstr + 2], a
     
     pop af
     ret
-
-;Exit a function while scheduling the return address as the next HBlank
-;handler.
-;
-;This routine has an unusual calling convention; you must have the original HL
-;that the interrupted code had on the stack. This code satisfies the exit
-;convention for a StatIRQ handler.
-LCDC_HBlankYield::
-    pop hl
-    ld a, l
-    ld [W_LCDC_HBlankInstr + 1], a
-    ld a, h
-    ld [W_LCDC_HBlankInstr + 2], a
-    
-    pop hl
-    pop af
-    reti
     
 ;Example code for an HBlank coroutine that splits the window layer across
 ;parts of the screen. You schedule .start at the top of the frame with an LYC
@@ -85,13 +67,11 @@ LCDC_HBlankYield::
 ;   ld [REG_SCY], a
 ;   ld a, $10
 ;   ld [REG_LYC], a
-;   push hl
-;   jp LCDC_HBlankYield
+;   M_LCDC_HBlankYield
 ; .start
 ;   ld a, $FF
 ;   ld [REG_SCY], a
 ;   ld a, $80
 ;   ld [REG_LYC], a
-;   push hl
-;   jp LCDC_HBlankYield
+;   M_LCDC_HBlankYield
 ;   jr .my_coroutine
