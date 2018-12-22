b2084796f927f477ef27d10b792605b5a4551bd9
David Wendt <dcrkid@yahoo.com>
2018-12-21 23:51:32 -0700

    Adjust and fix the test HBlank coroutine

--- src/game/stat_coroutine.asm
+++ src/game/stat_coroutine.asm
@@ -8,24 +8,34 @@ Game_StatIRQHandler::
     ld [REG_LYC], a
     M_LCDC_HBlankYield
     
-    ld a, $E0
+    ld a, $B0
     ld [REG_SCY], a
-    ld a, $40
+    ld a, $41
     ld [REG_LYC], a
     M_LCDC_HBlankYield
     
 .break_loop
-    ld a, $C0
+    ld a, $B3
     ld [REG_SCY], a
-    ld a, $BF
+    ld a, $B6
     ld [REG_SCY], a
-    ld a, $BE
-    ld [REG_SCY], a
-    ld a, $BD
+    ld a, $B9
     ld [REG_SCY], a
     ld a, $BC
     ld [REG_SCY], a
+    ld a, $BF
+    ld [REG_SCY], a
+    ld a, $C2
+    ld [REG_SCY], a
+    ld a, $B0
+    ld [REG_SCY], a
     ld a, [REG_LYC]
     inc a
+    cp M_LCDC_HDMARequestVblankScreenLine
+    jr z, .exit
     ld [REG_LYC], a
     M_LCDC_HBlankYieldJp .break_loop
+    
+.exit
+    pop af
+    reti
